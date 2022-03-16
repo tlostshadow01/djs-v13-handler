1b6e323fa1e594f26f32ed6415f8ef63fe625659
thelostshadow01 <101035482+thelostshadow01@users.noreply.github.com>
2022-03-16 16:43:05 +0300

    Update messageCreate.js

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -5,12 +5,9 @@ const settings = require(`../../botconfig/settings.json`);
 const { onCoolDown, replacemsg } = require("../../handlers/functions");
 const Discord = require("discord.js");
 
-
 module.exports = async (client, message) => {
 
-const db = 
-
-require("quick.db");
+const db = require("quick.db");
     
   var en = require("../../language/english");
 
@@ -74,9 +71,7 @@ require("quick.db");
           //if Command has specific permission return error
           if (command.memberpermissions && command.memberpermissions.length > 0 && !message.member.permissions.has(command.memberpermissions)) {
             return message.reply({ embeds: [new Discord.MessageEmbed()
-                .setColor(ee.wrongcolor)
-                /*.setFooter(ee.footertext, ee.footericon)*/
-                
+                .setColor(ee.wrongcolor)     
                 .setDescription(replacemsg(dil.sistem.yetki, {
                   command: command,
                   prefix: prefix
@@ -87,8 +82,6 @@ require("quick.db");
           if (command.requiredroles && command.requiredroles.length > 0 && message.member.roles.cache.size > 0 && !message.member.roles.cache.some(r => command.requiredroles.includes(r.id))) {
             return message.reply({embeds: [new Discord.MessageEmbed()
               .setColor(ee.wrongcolor)
-              .setFooter(ee.footertext, ee.footericon)
-              
               .setDescription(replacemsg(lang.system.rol, {
                 command: command,
                 prefix: prefix
@@ -99,9 +92,7 @@ require("quick.db");
           //if Command has specific users return error
           if (command.alloweduserids && command.alloweduserids.length > 0 && !command.alloweduserids.includes(message.author.id)) {
             return message.reply({embeds: [new Discord.MessageEmbed()
-              .setColor(ee.wrongcolor)
-              
-              
+              .setColor(ee.wrongcolor)   
               .setDescription(replacemsg(lang.system.user, {
                 command: command,
                 prefix: prefix
@@ -111,16 +102,14 @@ require("quick.db");
           //if command has minimum args, and user dont entered enough, return error
           if(command.minargs && command.minargs > 0 && args.length < command.minargs) {
             return message.reply({embeds: [new Discord.MessageEmbed()
-              .setColor(ee.wrongcolor)
-              
+              .setColor(ee.wrongcolor)           
               .setDescription(command.argsmissing_message && command.argsmissing_message.trim().length > 0 ? command.argsmissing_message : command.usage ? `${lang.system.usage}`  + command.usage : `${lang.system.wrong}`)]
             }).then(msg => {setTimeout(()=>{msg.delete().catch((e) => {console.log(String(e).grey)})}, settings.timeout.minargs)}).catch((e) => {console.log(String(e).grey)});
           }
           //if command has maximum args, and user enters too many, return error
           if(command.maxargs && command.maxargs > 0 && args.length > command.maxargs) {
             return message.reply({embeds: [new Discord.MessageEmbed()
-              .setColor(ee.wrongcolor)
-              
+              .setColor(ee.wrongcolor)          
               .setDescription(command.argstoomany_message && command.argstoomany_message.trim().length > 0 ? command.argstoomany_message : command.usage ? `${lang.system.usage}` + command.usage : `${lang.system.wrong}`)]
             }).then(msg => {setTimeout(()=>{msg.delete().catch((e) => {console.log(String(e).grey)})}, settings.timeout.maxargs)}).catch((e) => {console.log(String(e).grey)});
           }
@@ -128,8 +117,7 @@ require("quick.db");
           //if command has minimum args (splitted with "++"), and user dont entered enough, return error
           if(command.minplusargs && command.minplusargs > 0 && args.join(" ").split("++").filter(Boolean).length < command.minplusargs) {
             return message.reply({embeds: [new Discord.MessageEmbed()
-              .setColor(ee.wrongcolor)
-              
+              .setColor(ee.wrongcolor) 
               .setDescription(command.argsmissing_message && command.argsmissing_message.trim().length > 0 ? command.argsmissing_message : command.usage ? `${lang.system.usage}` + command.usage : `${lang.system.wrong}`)]
             }).then(msg => {setTimeout(()=>{msg.delete().catch((e) => {console.log(String(e).grey)})}, settings.timeout.minplusargs)}).catch((e) => {console.log(String(e).grey)});
           }
@@ -137,7 +125,6 @@ require("quick.db");
           if(command.maxplusargs && command.maxplusargs > 0 && args.join(" ").split("++").filter(Boolean).length > command.maxplusargs) {
             return message.reply({embeds: [new Discord.MessageEmbed()
               .setColor(ee.wrongcolor)
-              
               .setDescription(command.argstoomany_message && command.argstoomany_message.trim().length > 0 ? command.argsmissing_message : command.usage ? `${lang.system.usage} ` + command.usage : `${lang.system.wrong}`)]
             }).then(msg => {setTimeout(()=>{msg.delete().catch((e) => {console.log(String(e).grey)})}, settings.timeout.maxplusargs)}).catch((e) => {console.log(String(e).grey)});
           }
@@ -147,8 +134,7 @@ require("quick.db");
           /*if (lang.system.array)*/ {
             return message.reply({
               embeds: [new Discord.MessageEmbed()
-                .setColor(ee.wrongcolor)
-                
+                .setColor(ee.wrongcolor)       
                 .setTitle(replacemsg(lang.system.titlehata, {
                   prefix: prefix,
                   command: command
